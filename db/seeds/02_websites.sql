f5b9d1732840538500e049ed2a0f64a697f7d29d
Nader Nasr <nadernasr7@gmail.com>
2022-02-12 09:12:58 -0500

    db clean up

--- db/seeds/02_websites.sql
+++ db/seeds/02_websites.sql
@@ -1,5 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO websites (name, url, password, user_id, organization_id)
-              VALUES ('Alice', 'www.google.com', 'password', 1, 1);
-
--- INSERT INTO users (name, username, email, password) VALUES ('Kira');
+                VALUES ('Alice', 'www.google.com', 'password', 1, 1),
+                       ('Bob', 'www.facebook.com', 'password', 2, 2),
+                       ('Mike', 'www.meta.com', 'password', 3, 3),
+                       ('Jack', 'www.tsn.com', 'password', 4, 4);
--- db/seeds/03_organizations.sql
+++ db/seeds/03_organizations.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO organizations (name, user_id)
-           VALUES ('Lighthouse Labs', 1);
+INSERT INTO organizations (name, type, user_id)
+                   VALUES ('Lighthouse Labs', 'Work', 1),
+                          ('Facebook', 'Social Media', 2),
+                          ('Netflix', 'Entertainment', 3);
+
