7ef719be1c8810aa03acb873e75a9d56e1f5b513
Nader Nasr <nadernasr7@gmail.com>
2022-02-12 12:47:18 -0500

    test

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,21 @@
+/* eslint-disable no-undef */
 // Client facing scripts here
+$(() => {
+  console.log("ready");
+  $("#button").on('click', onClick);
+});
+
+const onClick = () => {
+  $.get("/api/websites")
+    .then((data) => {
+      const list = $("#list");
+      for (websites of data.websites) {
+        const li = `<li>${websites.url}</li>`;
+        list.append(li);
+      }
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+};
+
