64702102b0ed7282976f8f5a677dbf7bee0accc1
Nader Nasr <nadernasr7@gmail.com>
2022-02-12 19:48:30 -0500

    removed username from user db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, username, email, password)
-        VALUES ('Alice', 'Alice123', 'Alice@gmail.com', 'password'),
-               ('Bob', 'Bob123', 'Alice@gmail.com', 'password'),
-               ('Kira', 'Kira123', 'Kira@gmail.com', 'password'),
-               ('Sally', 'Sally123', 'Sally@gmail.com', 'password');
+INSERT INTO users (name, email, password)
+        VALUES ('Alice', 'Alice@gmail.com', 'password'),
+               ('Bob', 'Alice@gmail.com', 'password'),
+               ('Kira', 'Kira@gmail.com', 'password'),
+               ('Sally', 'Sally@gmail.com', 'password');
 
