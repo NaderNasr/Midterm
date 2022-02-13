f949602b34ddc6cc21a52322b4200024d183369a
Nader Nasr <nadernasr7@gmail.com>
2022-02-13 16:13:40 -0500

    changed name

--- db/seeds/02_websites.sql
+++ db/seeds/02_websites.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO websites (name, username, url, password, user_id)
-                VALUES ('Alice', 'user1', 'www.google.com', 'password', 1),
-                       ('Bob', 'user2', 'www.facebook.com', 'password', 2),
-                       ('Mike', 'user3', 'www.meta.com', 'password', 3),
-                       ('Jack', 'user4', 'www.tsn.com', 'password', 4);
+                VALUES ('Google', 'user1', 'www.google.com', 'password', 1),
+                       ('Facebook', 'user2', 'www.facebook.com', 'password', 2),
+                       ('Meta', 'user3', 'www.meta.com', 'password', 3),
+                       ('TSN', 'user4', 'www.tsn.com', 'password', 4);
