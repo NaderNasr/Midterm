c13cad42696242de870e24555ea2d99c1459e69f
Nader Nasr <nadernasr7@gmail.com>
2022-02-13 14:18:16 -0500

    added user name to website table

--- db/seeds/02_websites.sql
+++ db/seeds/02_websites.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO websites (name, url, password, user_id)
-                VALUES ('Alice', 'www.google.com', 'password', 1),
-                       ('Bob', 'www.facebook.com', 'password', 2),
-                       ('Mike', 'www.meta.com', 'password', 3),
-                       ('Jack', 'www.tsn.com', 'password', 4);
+INSERT INTO websites (name, username, url, password, user_id)
+                VALUES ('Alice', 'user1', 'www.google.com', 'password', 1),
+                       ('Bob', 'user2', 'www.facebook.com', 'password', 2),
+                       ('Mike', 'user3', 'www.meta.com', 'password', 3),
+                       ('Jack', 'user4', 'www.tsn.com', 'password', 4);
