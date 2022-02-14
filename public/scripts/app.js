04b12d3ef9dc8423a5e264ea7f74089c5e927344
Nader Nasr <nadernasr7@gmail.com>
2022-02-14 17:31:22 -0500

    generate password for client

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,68 @@
 /* eslint-disable no-undef */
+
+// const { Pool } = require("pg/lib");
+// const { user, password } = require("pg/lib/defaults");
+
 // Client facing scripts here
 $(() => {
-  console.log("ready");
-  $("#newVault").on('click', onClick);
+  const generate = document.querySelector(".generate");
+
+  const name = (type) => {
+    return document.querySelector("input[name=" + type + "]").checked;
+  };
+
+  generate.addEventListener("click", () => {
+    if (!document.querySelector("input[name=uppercase]").checked &&
+        !document.querySelector("input[name=number]").checked &&
+        !document.querySelector("input[name=lowercase]").checked &&
+        !document.querySelector("input[name=symbol]").checked) {
+      return alert('Please select a checkbox');
+    }
+
+    let password = "";
+    let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowercase = "abcdefghijklmnopqrstuvwxyz";
+    let number = "0123456789";
+    let symbol = "~*$%@#^&!?*'-=/,.{}()[]<>";
+
+    if (name("uppercase")) password += uppercase;
+    if (name("lowercase")) password += lowercase;
+    if (name("number")) password += number;
+    if (name("symbol")) password += symbol;
+
+
+    let result = "";
+    for (let i = 0; i < 10; i++) {
+      let num = Math.floor(Math.random() * password.length);
+      result += password[num];
+    }
+    console.log(result);
+    document.querySelector(".result").innerHTML = result;
+  });
 });
+// $("#validate").on('submit', isChecked);
 
-const onClick = () => {
-  $.get("/api/websites")
-    .then((data) => {
-      const list = $("#list");
-      for (users of data.users) {
-        const li = `
-          <h1>${users.name}</h1>
-          <h2>Name</h2>
-          <p>URL</p>
-          <p>Password</p>
-        `;
-        list.append(li);
-      }
-    })
-    .catch((err)=>{
-      console.log(err);
-    });
-};
+// Renders a new saved website password
+// const renderVault = () => {
+//   $.get("/api/websites")
+//     .then((data) => {
+//       const list = $("#list");
+//       for (website of data.website) {
+//         const li = `
+//           <h1>${users.name}</h1>
+//           <h2>Name</h2>
+//           <p>URL</p>
+//           <p>Password</p>
+//         `;
+//         list.append(li);
+//       }
+//     })
+//     .catch((err)=>{
+//       console.log(err);
+//     });
+// };
 
+
+
+// console.log(generateString(5)); // length
+// console.log(generateString(5, uppercase,  lowerCase, specialChars)); // length
